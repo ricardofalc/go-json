8317279c43c111339eb6974f1ba55ad44e4d3cff
Ricardo Falchi <90937422+ricardofalc@users.noreply.github.com>
2024-08-23 13:11:29 +0200

    feat: better data

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 )
 
 type Category struct {
@@ -40,14 +41,67 @@ func main() {
 		log.Fatalf("Failed to unmarshal JSON: %s", err)
 	}
 
+	var outputFileName string
+
+	outputFileName = "grouped_output.txt"
+	writeGroupedOutput(outputFileName, subcategories)
+
+	outputFileName = "original_output.txt"
+	writeOriginalOutput(outputFileName, subcategories)
+
+	fmt.Printf("Data has been successfully written to %s\n", outputFileName)
+}
+
+func writeGroupedOutput(fileName string, subcategories []Subcategory) {
 	// Create and open a file to write the output
-	outputFile, err := os.Create("output.txt")
+	outputFile, err := os.Create(fileName)
 	if err != nil {
 		log.Fatalf("Failed to create output file: %s", err)
 	}
 	defer outputFile.Close()
 
-	// Write the extracted data to the file
+	// Group subcategories by main category
+	categoryMap := make(map[string][]Subcategory)
+	for _, subcategory := range subcategories {
+		categoryMap[subcategory.Category.Name] = append(categoryMap[subcategory.Category.Name], subcategory)
+	}
+
+	// Get sorted main category names
+	var mainCategories []string
+	for category := range categoryMap {
+		mainCategories = append(mainCategories, category)
+	}
+	sort.Strings(mainCategories)
+
+	// Write the grouped data to the file
+	var count int
+
+	for i, mainCategory := range mainCategories {
+		output := fmt.Sprintf("%d. Main Category: %s\n", i+1, mainCategory)
+		output += "Subcategories:\n"
+
+		for _, subcategory := range categoryMap[mainCategory] {
+			output += fmt.Sprintf("- %s\n", subcategory.Name)
+			count++
+		}
+		output += "\n Total Subcategories: " + fmt.Sprintf("%d\n\n", count)
+		count = 0
+
+		_, err := outputFile.WriteString(output)
+		if err != nil {
+			log.Fatalf("Failed to write to output file: %s", err)
+		}
+	}
+}
+
+func writeOriginalOutput(fileName string, subcategories []Subcategory) {
+	// Create and open a file to write the output
+	outputFile, err := os.Create(fileName)
+	if err != nil {
+		log.Fatalf("Failed to create output file: %s", err)
+	}
+	defer outputFile.Close()
+
 	for i, subcategory := range subcategories {
 		output := fmt.Sprintf("Category #%d\n", i+1)
 		output += fmt.Sprintf("Main Category: %s\n", subcategory.Category.Name)
@@ -58,6 +112,4 @@ func main() {
 			log.Fatalf("Failed to write to output file: %s", err)
 		}
 	}
-
-	fmt.Println("Data has been successfully written to output.txt")
 }
